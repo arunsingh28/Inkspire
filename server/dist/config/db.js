72a58c055aa13f1c921244b47f52c014567e6578
Arun Singh <arunsingh28aug.as@gmail.com>
2024-12-20 21:57:04 +0530

    buid

--- server/dist/config/db.js
+++ server/dist/config/db.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectDB = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const connectDB = async () => {
-    const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/blog-app";
+    const MONGO_URI = process.env.MONGO_PROD_URI || "mongodb://localhost:27017/blog-app";
     try {
         await mongoose_1.default.connect(MONGO_URI);
         console.log("Database connected successfully");
